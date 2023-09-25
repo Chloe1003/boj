b63d1b0e68e3b5f93a1ee7288adb1ba28f89304d
Chloe1003 <47780936+Chloe1003@users.noreply.github.com>
2023-09-25 11:08:47 +0900

    230925 문제풀이

--- Basic/집합과 맵/1620.py
+++ Basic/집합과 맵/1620.py
@@ -1,8 +1,14 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 p_dict = {}
 for i in range(1, n+1):
-    p_dict[input()] = i
-p_dict_reverse = {val:key for key, val in p_dict.items()}
+    p = input().rstrip()
+    p_dict[p] = i
+    p_dict[i] = p
 for _ in range(m):
-    name = input()
-    print(p_dict.get(name) if name in p_dict else p_dict_reverse.get(name))
+    name = input().rstrip()
+    if name.isdigit():
+        print(p_dict[int(name)])
+    else:
+        print(p_dict[name])
